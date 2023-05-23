a7f882f2cf67228274ab21c88e513ce8b12a5011
Luca Kaesmann <l.kaesmann@oth-aw.de>
2023-05-23 12:48:25 +0200

    Daten als JSON speichern, wenn nicht aktuell
    Former-commit-id: 742acdf523431d37810a3eadd7e7ffaae4b06e1d

--- sys-src/stock_data.py
+++ sys-src/stock_data.py
@@ -3,50 +3,30 @@ import pandas as pd
 from datetime import date
 
 
-def get_data(stock_symbol):
+def get_data(stock_symbol, path):
     # check if data is updated
-    # if is_data_updated(stock_symbol):
-    #    return
+    if is_data_updated(stock_symbol, path):
+        print("no update")
+        return
 
-    #else:
-    ticker = yfinance.Ticker(stock_symbol)
-    data = ticker.history(period='max', interval='1d')
-
-    df = pd.DataFrame(data)
-    print(df)
-
-    #try:
-    #    df.to_csv("C:/BKI/04_SOSE 2023/Big Data, Cloud and NoSQL/Stockbird/yFinanceData/" + stock_symbol + ".csv")
-        #update_data_info(stock_symbol)
-    #except PermissionError:
-    #    print("The csv file might be opened on your system.")
-    #    return
-
-    #update_data_info(stock_symbol)
+    else:
+        print("update")
+        # get data and save in a dataframe
+        ticker = yfinance.Ticker(stock_symbol)
+        data = ticker.history(period='max', interval='1d')
+        return pd.DataFrame(data)
 
 
+def is_data_updated(stock_symbol, path):
+    # try to find data on local machine
+    try:
+        df = pd.read_json(path)
+        df.index = df.index.strftime('%Y-%m-%d')
 
-def update_data_info(stock_symbol):
-    data_info = pd.read_csv("C:/BKI/04_SOSE 2023/Big Data, Cloud and NoSQL/Stockbird/yFinanceData/data_information.csv")
+        return df.index[-1] == str(date.today())
 
-    # iterate through the dataframe and update the date
-    for ind in data_info.index:
-        if data_info["symbol"][ind] == stock_symbol:
-            data_info["updated"][ind] = date.today()
-            return
-
-    data_info.append({"symbol": stock_symbol, "updated": date.today()})
-    data_info.to_csv("C:/BKI/04_SOSE 2023/Big Data, Cloud and NoSQL/Stockbird/yFinanceData/data_information.csv")
+    except FileNotFoundError:
+        return False
 
 
-def is_data_updated(stock_symbol):
-    data_info = pd.read_csv("C:/BKI/04_SOSE 2023/Big Data, Cloud and NoSQL/Stockbird/yFinanceData/data_information.csv")
 
-    for ind in data_info.index:
-        if data_info["symbol"][ind] == stock_symbol:
-            if data_info["updated"][ind] == date.today():
-                return True
-            else:
-                return False
-
-    return False
