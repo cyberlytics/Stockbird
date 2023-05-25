5aa0a1987a18720eb40fedafe0f7969b88957213
Carl Kueschall <c.kueschall@oth-aw.de>
2023-05-25 13:59:14 +0200

    bin Fertig fuer immer
    Former-commit-id: 2ee8ebd6bb48d0737d0a9948a06ea5f9db84da0c

--- sys-src/frontend/src/App.js
+++ sys-src/frontend/src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 
 function App() {
+    var callAPI = (symbol)=>{
+        // instantiate a headers object
+        var myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+        var raw = JSON.stringify({"symbol":symbol});
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        // make API call with parameters and use promises to get response
+        fetch("https://szlw5m95d9.execute-api.eu-central-1.amazonaws.com/dev", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(JSON.parse(result).body))
+            .catch(error => console.log('error', error));
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Hello There</h1>
-      </header>
-    </div>
+
+        <div className="App">
+          <header className="App-header">
+
+          </header>
+            <body>
+            <h1>Stock Selector!</h1>
+            <form>
+                <label>Symbol:</label>
+                <input type="text" id="symbol"></input>
+                <button type="button" onClick= {callAPI("META")}>CALCULATE</button>
+            </form>
+            </body>
+        </div>
   );
 }
 
