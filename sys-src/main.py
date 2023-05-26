0b3153731ab1cc3a13e248f373aa390c811db221
Luca Kaesmann <l.kaesmann@oth-aw.de>
2023-05-26 15:30:56 +0200

    Test get_data
    Former-commit-id: d324aec80e2af11d8ff4689497fb47b23d770559

--- sys-src/main.py
+++
@@ -1,9 +0,0 @@
-import stock_data
-
-if __name__ == '__main__':
-    # TODO: When Johnny is done with Symbol list, check if Symbol is available
-    stock_symbol = "MSF.DE"
-    path = "C:/BKI/04_SOSE 2023/Big Data, Cloud and NoSQL/Stockbird/yFinanceData/" + stock_symbol + ".json"
-
-    df = stock_data.get_data(stock_symbol, path)
-    df.to_json(path)
---
+++ sys-src/stock/test/test_stock_data.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+
+from source import stock_data
+
+
+class TestStockData(unittest.TestCase):
+    def test_get_data(self):
+        stock_symbol = "MSF.DE"
+        path = "../data/" + stock_symbol + ".json"
+        stock_data.get_data(stock_symbol, path)
+
+        self.assertTrue(os.path.exists(path))
+
+        stock_symbol = "MSFT"
+        path = "../data/" + stock_symbol + ".json"
+        stock_data.get_data(stock_symbol, path)
+
+        self.assertTrue(os.path.exists(path))
+
+    def test_is_dat_updated(self):
+        stock_symbol = "MSF.DE"
+        path = "../data/" + stock_symbol + ".json"
+
+        self.assertTrue(stock_data.is_data_updated(path))
+
+        stock_symbol = "APPL"
+        path = "../data/" + stock_symbol + ".json"
+
+        self.assertFalse(stock_data.is_data_updated(path))
