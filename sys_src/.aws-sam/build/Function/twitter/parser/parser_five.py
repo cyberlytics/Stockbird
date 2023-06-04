57cb97193e41a26076096e23f0224e9817e9e11b
Carl Kueschall <c.kueschall@oth-aw.de>
2023-06-04 21:17:03 +0200

    not sure
    Former-commit-id: 00e610a625c1fdaa04ab7cddfab93f5a144c2d33

---
+++ sys_src/.aws-sam/build/Function/twitter/parser/parser_five.py
@@ -0,0 +1,38 @@
+import argparse
+from abstract_parser import *
+from sys_src.backend.src.Constants import TweetColumns
+
+
+class ParserFive(AbstractParser):
+
+    def __init__(self, source_path: Path):
+        super().__init__(source_path)
+
+        self._remove_unused_columns(['user_location', 'user_description',
+                                     'user_created', 'user_friends', 'user_favourites',
+                                     'hashtags', 'source', 'is_retweet'])
+        self._rename_columns({'date': TweetColumns.TIMESTAMP.value})
+        self._add_missing_columns([TweetColumns.RETWEETS.value])
+        self._change_timestamp_format()
+        self._change_column_order([i.value for i in TweetColumns])
+
+
+def add_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i',
+                        '--input-path',
+                        type=str,
+                        required=True,
+                        help='Path to the file to be parsed'
+                        )
+    return parser.parse_args()
+
+
+def main():
+    args = add_args()
+    parser = ParserFive(Path(args.input_path))
+    parser.append_to_file()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sys_src/.aws-sam/build/Function/twitter/parser/parser_two.py
@@ -0,0 +1,37 @@
+import argparse
+from abstract_parser import *
+from sys_src.backend.src.Constants import TweetColumns
+
+
+class ParserTwo(AbstractParser):
+
+    def __init__(self, source_path: Path):
+        super().__init__(source_path)
+
+        self._remove_unused_columns(['id', 'url', 'company_names', 'symbols'])
+        self._rename_columns({'verified': TweetColumns.USERVERIFIED.value,
+                              'source': TweetColumns.USERNAME.value})
+        self._add_missing_columns([TweetColumns.RETWEETS.value, TweetColumns.USERFOLLOWERS.value])
+        self._change_timestamp_format('%a %b %d %H:%M:%S %z %Y')
+        self._change_column_order([i.value for i in TweetColumns])
+
+
+def add_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i',
+                        '--input-path',
+                        type=str,
+                        required=True,
+                        help='Path to the file to be parsed'
+                        )
+    return parser.parse_args()
+
+
+def main():
+    args = add_args()
+    parser = ParserTwo(Path(args.input_path))
+    parser.append_to_file()
+
+
+if __name__ == "__main__":
+    main()
