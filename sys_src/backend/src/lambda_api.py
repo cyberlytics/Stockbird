11cdd1dd754c599cf8e160db56a58774df2b20fd
Luca Kaesmann <l.kaesmann@oth-aw.de>
2023-06-07 12:51:41 +0200

    Test Anpassungen und Filterung nach Datum
    Former-commit-id: 7d43bd7ab2373096dfbd62e1449b304d81034bc3

--- sys_src/backend/src/lambda_api.py
+++ sys_src/backend/src/lambda_api.py
@@ -13,6 +13,16 @@ def _get_stock_data(event):
         'body': df_json_str
     }
 
+def _filter_stock_by_date(event):
+    df_json = stock_data.get_data(event['symbol'], f"stock_{event['symbol']}.json")
+    df_filtered = stock_data.filter_by_date(df_json, event['from_date'], event['to_date'])
+    df_json_str = json.dumps(df_filtered)
+
+    return {
+        'statusCode': 200,
+        'body': df_json_str
+    }
+
 
 def _query_tweets_by_substring(event):
     df_json = tweet_utils.query_tweets_by_substring(const.TWEETS_FILENAME, event['substring'])
