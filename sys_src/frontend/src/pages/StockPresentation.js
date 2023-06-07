b03b4a64f982ba8a9672bc4e9de16a7e3a883916
Baran Baygin <b.baygin@oth-aw.de>
2023-06-07 21:01:38 +0200

    Kommentare hinzugefügt
    Former-commit-id: 05c3e02b8400ae1501ad11d97bee52b802997bd7

--- sys_src/frontend/src/pages/StockPresentation.js
+++ sys_src/frontend/src/pages/StockPresentation.js
@@ -5,17 +5,29 @@ import * as d3 from 'd3';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { callAPITweets } from './api'; // Import the callAPI function
 
 export default function StockPresentation() {
+
+    //location contains a JSON-Files, the stock-data and the symbol of the Stock
     const location = useLocation();
+    //Both can be accessed with location.state
+    //parsedResult2 contains the stockData and symbol is the Symbol name of the Stock (Used for visualizing and mapping)
     const { parsedResult2, symbol } = location.state;
+    //jsonDataString is a string which contains the stockData as a String, which has to be converted into a JSON-File
     const jsonDataString = parsedResult2;
+    //jsonData contains the stock-data as a jsonType Object
     const jsonData = JSON.parse(jsonDataString);
 
+    //Both tweetData and data is used to store the tweets which we get from api.js
+    //state allows to store and manage data that can change over time, which in this case does everytime when the user wants to see new Stocks
     const [tweetData, setTweetData] = useState('');
+    const [data, setData] = useState('');
 
+    //useRef allows the manipulation of the DOM, where we plot the stock if data is available
     const chartRef = useRef(null);
 
+    //here we map our symbols to the corresponding name of the Company which is necessary for receiving the right tweets
     const symbolMapping = {
         TSLA: 'Tesla',
         AAPL: 'Apple',
@@ -24,31 +36,35 @@ export default function StockPresentation() {
         'BTC-USD': 'Bitcoin'
     };
 
+    //in this variable we store in the symbol because in the api.js we need a substring for the backend for filtering the tweets where for example 'Dogecoin' is included the tweet
     const secondParameter = symbolMapping[symbol];
 
-    const [data, setData] = useState('');
-
+    //draws the chart when the stock-data is available
     useEffect(() => {
-        //Wenn die Daten verfügbar sind dann zeichne den Graphen
         if (jsonData) {
             drawChart();
         }
-    }, []); // Pass `jsonData` as a dependency to the useEffect hook
+        //Here if the json-data would change then we could pass in here the jsonData but that's not necessary otherwise it will get drawn twice if we press on the 'analyze' button
+    }, []);
 
     const drawChart = () => {
-
+        //'Close' includes the information we want to plot to the screen
+        //Other attributes are for example: Open,High,Low,Volume,Dividends,Stock Splits
         const jsonDataClose = jsonData.Close;
         // Die Zeitangaben in der JSON-Datei sind vom Typ UNIX und werden entsprechend umgewandelt
+        //the timestamps have the type of UNIX, with this map function we iterate through each element and parse the values from the 'Close' attribute
         const data = Object.entries(jsonDataClose).map(([timestamp, value]) => ({
             timestamp: parseInt(timestamp),
             value: parseFloat(value),
+            //filtering data where the data is NaN
         })).filter(dataPoint => !isNaN(dataPoint.value));
 
-        // Styling festlegen
+        // Some Styling of the d3 plot
         const margin = { top: 70, right: 30, bottom: 30, left: 40 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
 
+        //here we select the chartRef as we assigned it before to manipulate the DOM
         const svg = d3
             .select(chartRef.current)
             .append('svg')
@@ -57,22 +73,26 @@ export default function StockPresentation() {
             .append('g')
             .attr('transform', `translate(${margin.left},${margin.top})`);
 
+        //x-axis is being scaled based on the timestamps in the data
         const xScale = d3
             .scaleTime()
             .domain(d3.extent(data, d => d.timestamp))
             .range([0, width]);
 
+        //y-axis is being scaled based on the values in the data
         const yScale = d3
             .scaleLinear()
             .domain(d3.extent(data, d => d.value))
             .range([height, 0]);
 
+        //creating the x-axis and y-axis
         const xAxis = d3.axisBottom(xScale);
         const yAxis = d3.axisLeft(yScale);
 
         svg.append('g').attr('transform', `translate(0,${height})`).call(xAxis);
         svg.append('g').call(yAxis);
 
+        //plot should be a line
         const line = d3
             .line()
             .x(d => xScale(d.timestamp))
@@ -87,39 +107,20 @@ export default function StockPresentation() {
             .attr('d', line);
     };
 
-    const callAPI = async (control, substring) => {
-        // instantiate a headers object
-        var myHeaders = new Headers();
-        // add content type header to object
-        myHeaders.append('Content-Type', 'application/json');
-        // using built-in JSON utility package turn object to string and store in a variable
-        var raw = JSON.stringify({ control: control, substring: substring });
-        // create a JSON object with parameters for API call and store in a variable
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow',
-        };
-
-        // make API call with parameters and use promises to get response
-        fetch('https://szlw5m95d9.execute-api.eu-central-1.amazonaws.com/dev', requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                try {
-                    const parsedResult = JSON.parse(result).body;
-                    const parsedResult2 = JSON.parse(parsedResult);
-                    if (control === '_query_tweets_by_substring') {
-                        setTweetData(parsedResult2);
-                        const jsonDataTweet = JSON.parse(tweetData);
-                        setData(jsonDataTweet.data)
-                        console.log(data);
-                    }
-                } catch (error) {
-                    console.log('Error parsing JSON:', error);
-                }
-            })
-            .catch(error => console.log('Error fetching data:', error));
+    //this function gets triggered when the 'Analyze' button is clicked
+    //with this function we call the 'callAPITweets' which is defined in the 'api.js'
+    //we store all the information we get from the 'callAPITweets' in the jsonDataTweet
+    const handleAnalyzeClick = async () => {
+        try {
+            const apiTweets = await callAPITweets('_query_tweets_by_substring', secondParameter);
+            //here we set the tweetData value to the tweets we get back, but here are some information we don't need like
+            setTweetData(apiTweets);
+            const jsonDataTweet = JSON.parse(tweetData);
+            //here we only select the 'data' attribute and not the column names
+            setData(jsonDataTweet.data);
+        } catch (error) {
+            console.log('Error during API call:', error);
+        }
     };
 
     return (
@@ -128,7 +129,7 @@ export default function StockPresentation() {
                 <img className="Centered-img" id="splogo" src={logo} alt="Stockbird Logo" />
                 <Typography variant="h3">{symbol}</Typography>
                 <div ref={chartRef}></div>
-                <Button variant="contained" onClick={() => callAPI('_query_tweets_by_substring', secondParameter)}>
+                <Button variant="contained" onClick={handleAnalyzeClick}>
                     Analyze
                 </Button>
             </div>
@@ -146,7 +147,6 @@ export default function StockPresentation() {
                     </>
                 )}
             </div>
-
         </>
     );
 }
