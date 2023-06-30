1e2beec43336bb4dafc981764d44d948243311e2
Rebecca Kietzer <r.kietzer@oth-aw.de>
2023-06-30 17:31:14 +0200

    Sort by influence, Bugfixing, small design changes
    Former-commit-id: 9cf681c6c3bcefc451e1e594e1e296b42007b941

--- sys_src/frontend/src/pages/api.js
+++ sys_src/frontend/src/pages/api.js
@@ -1,10 +1,17 @@
 //function makes an API call to retrieve tweet data based on the specified control and substring
 //the substring is given in the 'StockPresentation', it depents on what the user wants to see, so if he types 'TSLA' the symbolMapping takes care that 'Tesla' is here the symbol
 //function returns 'parsedResult2' which contains the tweet-data we need to work with
-export const callAPITweets = async (control, date_from, date_to, substrings) => {
+export const callAPITweets = async (control, date_from, date_to, substrings, symbol) => {
     var myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
-    var raw = JSON.stringify({ control: control, date_from: date_from, date_to: date_to, substrings: substrings });
+    var raw;
+    if(date_from === null && date_to === null) 
+    {
+        raw = JSON.stringify({ control: control, substrings: substrings, symbol: symbol });
+    }
+    else {
+        raw = JSON.stringify({ control: control, date_from: date_from, date_to: date_to, substrings: substrings, symbol: symbol });
+    }
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
@@ -17,9 +24,8 @@ export const callAPITweets = async (control, date_from, date_to, substrings) => 
         const parsedResult = JSON.parse(result).body;
         //important to parse it again, otherwise there will be problems with the return type
         const parsedResult2 = JSON.parse(parsedResult);
-        if (control === '_query_tweets_by_stock') {
-            return parsedResult2;
-        }
+        console.log(parsedResult2);
+        return parsedResult2;
     }catch(error){
         console.log('Error fetching data:', error);
         throw error;
